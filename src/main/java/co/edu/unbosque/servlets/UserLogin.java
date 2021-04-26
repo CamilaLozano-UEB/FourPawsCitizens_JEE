2d53a0d6b87d7f15ea8d0f85b12e699af97c51c5
JuanaTorres <jvtorresp@unbosque.edu.co>
2021-04-26 18:26:07 -0500

    operation in change of index Juana Torrres

--- src/main/java/co/edu/unbosque/servlets/UserLogin.java
+++ src/main/java/co/edu/unbosque/servlets/UserLogin.java
@@ -5,33 +5,24 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 
 @WebServlet(name = "loginWithCookies", value = "/login-with-cookies")
 public class UserLogin extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-        response.setContentType("text/html");
+		response.setContentType("text/html");
 
-        String name = request.getParameter("userName");
-        try {
+		String name = request.getParameter("userName");
+		try {
+			// Creating two cookies
+			Cookie c1 = new Cookie("userName", name);
+			// Adding the cookies to response header
+			response.addCookie(c1);
+		} catch (Exception exp) {
+			System.out.println(exp);
+		}
 
-            PrintWriter out = response.getWriter();
-            out.println("Hello " + name);
-            out.println("<br />");
-            //Creating two cookies
-            Cookie c1 = new Cookie("userName", name);
-            //Adding the cookies to response header
-            response.addCookie(c1);
-
-            out.println("<a href='welcome'>View Details</a>");
-            out.close();
-
-        } catch(Exception exp) {
-            System.out.println(exp);
-        }
-
-    }
+	}
 
 }
